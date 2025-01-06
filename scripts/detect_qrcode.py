8eaa7b3fd78e497233718db2f8da7fe7410be56a
kenta-kawamata <university.kawamata@gmail.com>
2025-01-06 14:39:50 +0000

    add decoder use zbar

--- scripts/detect_qrcode.py
+++ scripts/detect_qrcode.py
@@ -1,8 +1,11 @@
 #! /usr/bin/env python3
+from PIL import Image
+import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from skimage.feature import hog
 from skimage import data, exposure
+from pyzbar.pyzbar import decode, ZBarSymbol
 
 def output_hog_featurers(image):
 
@@ -58,12 +61,51 @@ def detect_qr(img):
     return trimed_img
 
 
+def detect_qr_using_pyzbar(img):
+
+    decoded_list = decode(img)
+
+    print(type(decoded_list))
+    # <class 'list'>
+    
+    print(len(decoded_list))
+    # 3
+    
+    print(type(decoded_list[0]))
+    # <class 'pyzbar.pyzbar.Decoded'>
+    
+    print(decoded_list[0])
+
+
+def pil2cv(image):
+
+    ''' PIL型 -> OpenCV型 '''
+    new_image = np.array(image, dtype=np.uint8)
+
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+
+    elif new_image.shape[2] == 4:  # 透過
+
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
+
+    return new_image
+
 def main():
 
-    img = cv2.imread('./../qr_code.png')
+    #img = cv2.imread('./../qr_code.png')
+    img = Image.open('./../qr_code.png')
+    img = pil2cv(img)
 
-    trimed_img = detect_qr(img)
-    output_hog_featurers(trimed_img)
+    img = cv2.resize(img,None,fx=0.4,fy=0.4)
+
+    detect_qr_using_pyzbar(img)
+
+    #trimed_img = detect_qr(img)
+    #output_hog_featurers(trimed_img)
 
     #cv2.imshow("qr code",trimed_img)
     #cv2.waitKey(0)
